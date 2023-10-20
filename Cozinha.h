bd72f825bfb47d7e0817d059f3b75267f2fac167
Augustodalpra12 <augusto_dalpra@hotmail.com>
2023-10-20 20:34:56 -0300

    Teste 2

--- Cozinha.h
+++ Cozinha.h
@@ -5,35 +5,37 @@ using namespace chrono;
 
 class Cozinha{
     public:
-        Cozinha();
         ~Cozinha();
 
         //get 6 alimentos
+
         int getSalad();
         int getDessert();
         int getRice();
         int getBean();
-        int getComplement();
+        // alimento variado
+        int getComplement(); 
+
         int getMeat();
 
         //set 6 alimentos
 
-        int setSalad();
-        int setDessert();
-        int setRice();
-        int setBean();
-        int setComplement();
-        int setMeat();
+        void setSalad(int valor);
+        void setDessert(int valor);
+        void setRice(int valor);
+        void setBean(int valor);
+        void setComplement(int valor);
+        void setMeat(int valor);
 
 
     private:
         // 6 alimentos: Salada, Sobremesa, Arroz, Feijão, Complemento, Carne
         const int productionTime[6] = {10, 5, 20, 20, 35, 50};
         const int productionTimeVariance[6] = {2, 1, 3, 3, 10, 5};
-        const int readyForDelivery[6];
+        int readyForDelivery[6];
 
         // preparo de itens
-        int cookingItems(int food);
+        void cookingItems(int food);
         
         // entrega de itens
         int deliveryItems(int food);
