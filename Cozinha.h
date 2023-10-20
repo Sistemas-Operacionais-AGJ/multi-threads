df2e7d00196bf1b49dabb4b9c126002641ddedf9
Augustodalpra12 <augusto_dalpra@hotmail.com>
2023-10-20 20:51:41 -0300

    🐛 Fix a Bug

--- Cozinha.h
+++ Cozinha.h
@@ -27,18 +27,15 @@ class Cozinha{
         void setComplement(int valor);
         void setMeat(int valor);
 
-
-    private:
-        // 6 alimentos: Salada, Sobremesa, Arroz, Feijão, Complemento, Carne
-        const int productionTime[6] = {10, 5, 20, 20, 35, 50};
-        const int productionTimeVariance[6] = {2, 1, 3, 3, 10, 5};
-        int readyForDelivery[6];
-
         // preparo de itens
         void cookingItems(int food);
         
         // entrega de itens
         int deliveryItems(int food);
 
-
+    private:
+        // 6 alimentos: Salada, Sobremesa, Arroz, Feijão, Complemento, Carne
+        const int productionTime[6] = {10, 5, 20, 20, 35, 50};
+        const int productionTimeVariance[6] = {2, 1, 3, 3, 10, 5};
+        int readyForDelivery[6];
 };
